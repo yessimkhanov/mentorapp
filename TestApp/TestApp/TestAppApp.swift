007222403d2bd78b82cfd3414bf6d9fc89861eed
aldiyar <aldiyar.yessimkhanov@nu.edu.kz>
2024-11-19 11:24:00 +0500

    first

---
+++ TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Nursultan Turekulov on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
